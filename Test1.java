a9e3f0936f3df629f1d25943d65f719c80d42549
Jieling-Cai <93742984+Jieling-Cai@users.noreply.github.com>
2022-08-08 11:14:13 -0400

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -13,26 +13,16 @@ public class Test1 {
         mbta.loadConfig("sample2.json");
         Log log = new Log();
         StopSignal stop = new StopSignal(mbta);
-        //TrainThread trainThread = new TrainThread(log,mbta,Train.make("red"));
         Thread t1 = new Thread(new TrainThread(log,mbta,Train.make("orange"),stop));
         Thread t2 = new Thread(new TrainThread(log,mbta,Train.make("red"),stop));
-        //Thread t3 = new Thread(new TrainThread(log,mbta,Train.make("blue"),stop));
-        //Thread t4 = new Thread(new TrainThread(log,mbta,Train.make("green"),stop));
         Thread t5 = new Thread(new PassengerThread(log,mbta,Passenger.make("Bob"),stop));
-        //Thread t6 = new Thread(new PassengerThread(log,mbta,Passenger.make("Alice"),stop));
-        //Thread t7 = new Thread(new PassengerThread(log,mbta,Passenger.make("Carol"),stop));
         Thread t8 = new Thread(stop);
         List<Thread> tt = new ArrayList<>();
 
         tt.add(t1);
         tt.add(t2);
-        //tt.add(t3);
-        //tt.add(t4);
         tt.add(t5);
-        //tt.add(t6);
-        //tt.add(t7);
         tt.add(t8);
-        //Thread t2 = new Thread(trainThread);
         for(Thread t: tt){
             t.start();
         }
