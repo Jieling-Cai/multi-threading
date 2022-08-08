4954526b0573986d1a938ce9aafc4d3842811ab0
Jieling-Cai <93742984+Jieling-Cai@users.noreply.github.com>
2022-08-08 11:13:33 -0400

    Update StopSignal.java

--- StopSignal.java
+++ StopSignal.java
@@ -17,32 +17,18 @@ public class StopSignal implements Runnable{
 
             while (!stopsignal) {
                 int passengers_finished_old = 0;
-
-                //System.out.println(mbta.passengers_finished.size());
                 try {
                     sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                /*if (passengers_finished_old == mbta.passengers_finished.size())
-                {
-                    time_counter = time_counter+1;
-                }
-                else
-                {
-                    time_counter = 0;
-                    passengers_finished_old = mbta.passengers_finished.size();
-                }*/
                 time_counter++;
                 if ((mbta.passengers_finished.size() == mbta.all_journey.size())||time_counter>=120) {
                     stopsignal = true;
-                    //System.out.println(mbta.passengers_finished.size());
-
                     System.out.println(" All Finish");
                     break;
                 }
             }
-            //Thread.interrupted();
         }
             catch(RuntimeException e){
                 Thread.interrupted();
